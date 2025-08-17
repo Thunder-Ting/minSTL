0a1115947afc47cc81be2ca1973a495be7781610
Romml <2863731108@qq.com>
2025-08-17 18:47:05 +0800

    xxx

--- main.cpp
+++ main.cpp
@@ -2,20 +2,13 @@
 #include <vector.h>
 #include <string>
 #include <vector>
+#include <algorithm>
+#include <memory>
+#include <type_traits>
 using std::cout;
 using std::endl;
 using std::string;
 
-struct KV {
-    string key;
-    string value;
-};
-
 int main() {
-    // leistd::vector<KV> v;
-    std::vector<int> v = {1, 2, 3};
-    v.insert(v.begin(), 4);
-    v.insert(v.begin() + 1, {99, 99, 99});
-
     return 0;
 }
